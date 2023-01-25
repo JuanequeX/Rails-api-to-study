7960f703cccc67793280a71da0afdf48582d7581
juanequex <ramon.g.uz37@gmail.com>
2023-01-24 20:49:04 -0600

    Create Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
